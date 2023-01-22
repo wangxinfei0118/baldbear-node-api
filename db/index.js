8cd9eb352c29dfbbdb06f455602c6e65ad7715dd
wxf <1019072635@qq.com>
2023-01-22 23:16:17 +0800

    feat: 建立mysql数据库连接

---
+++ db/index.js
@@ -0,0 +1,12 @@
+// 导入 mysql 模块
+const mysql = require('mysql')
+
+// 创建数据库连接对象
+const db = mysql.createPool({
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'root',
+  database: 'baldbear',
+})
+
+module.exports = db
