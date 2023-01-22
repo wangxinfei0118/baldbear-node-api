1888baffdb8a4a67c8ec7236e821d35fbcbb7c0c
wxf <1019072635@qq.com>
2023-01-22 23:20:20 +0800

    chore: 添加cors模块并配置

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
+
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
 
 
 app.listen(3008, function () {
