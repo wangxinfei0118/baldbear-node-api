b8cad5ba2a3eb9bfeb487a144ee2d926e3d5f52f
wxf <1019072635@qq.com>
2023-01-23 23:47:57 +0800

    feat: 定义查询用户名处理函数

--- router_handler/auth.js
+++ router_handler/auth.js
@@ -1,9 +1,19 @@
 const db = require('../db/index')
 
 
-
 exports.username = (req, res) => {
-
+  const username = req.params.username
+  const sql_username = `select * from bb_users where username=?`
+  db.query(sql_username,username,(err,results) => {
+    if (err) {
+      return res.err(err)
+    }
+    if (results.length > 0) {
+      return res.send({ code: 20000, message: '用户名已存在', data: username })
+    }
+    // 用户名可用
+      res.send({ code: 20000, message: '用户名可用' })
+  })
 }
 
 
