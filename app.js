c98ce73c7460e99d2254649286fe6f14feba9db4
wxf <1019072635@qq.com>
2023-01-23 23:37:33 +0800

    feat: 应用express.json处理body中的json数据

--- app.js
+++ app.js
@@ -8,9 +8,12 @@ const app = express()
 
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 
 app.use('/auth', authRouter)
+
+
 
 app.listen(3008, function () {
   console.log('api server running at http://127.0.0.1:3008')
