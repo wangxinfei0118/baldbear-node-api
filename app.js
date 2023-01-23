74629cb5b04393dc00f5142f6fca9a3e923f4d41
wxf <1019072635@qq.com>
2023-01-23 23:36:00 +0800

    feat: 定义登陆注册相关路由，并应用joi验证规则

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const cors = require('cors')
+const joi = require('joi')
+
+const authRouter = require('./router/auth')
 
 const app = express()
 
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 
+
+app.use('/auth', authRouter)
 
 app.listen(3008, function () {
   console.log('api server running at http://127.0.0.1:3008')
