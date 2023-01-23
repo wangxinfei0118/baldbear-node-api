2395561c737589a557362cc1761c1f931af9c251
wxf <1019072635@qq.com>
2023-01-23 23:46:12 +0800

    feat: 封装路由处理模块，定义处理函数并应用

--- router/auth.js
+++ router/auth.js
@@ -1,23 +1,18 @@
 const express = require('express')
 const expressJoi = require('@escook/express-joi')
 
+const authHandler = require('../router_handler/auth')
 const { reg_schema, login_schema } = require('../schema/auth')
 
 const router = express.Router()
 
 // 检测用户名是否存在
-router.get('/username/:username', function () {
-
-})
+router.get('/username/:username', authHandler.username)
 
 // 注册
-router.post('/register', expressJoi(reg_schema), function () {
-
-})
+router.post('/register', expressJoi(reg_schema), authHandler.register)
 
 // 登录
-router.post('/login', expressJoi(login_schema), function () {
-
-})
+router.post('/login', expressJoi(login_schema), authHandler.login)
 
 module.exports = router
---
+++ router_handler/auth.js
@@ -0,0 +1,16 @@
+const db = require('../db/index')
+
+
+
+exports.username = (req, res) => {
+
+}
+
+
+exports.register = (req, res) => {
+
+}
+
+exports.login = (req, res) => {
+
+}
