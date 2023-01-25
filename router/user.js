015e0537f4d476f0e4f2a7a132cccebc162b700d
wxf <1019072635@qq.com>
2023-01-25 23:51:52 +0800

    feat: 添加用户信息修改，密码修改，头像修改接口路由，并应用校验规则

--- router/user.js
+++ router/user.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const expressJoi = require('@escook/express-joi')
 
 const userHandler = require('../router_handler/user')
+const { userinfo_schema, update_password_schema, update_avatar_schema } = require('../schema/user')
 
 const router = express.Router()
 
 router.get('/user/:uid', userHandler.getUserInfo)
+
+router.put('/user/:uid', expressJoi(userinfo_schema), userHandler.updateUserInfo)
+
+router.put('/password/:uid', expressJoi(update_password_schema), userHandler.updatePassword)
+
+router.put('/avatar/:uid', expressJoi(update_avatar_schema), userHandler.updateAvatar)
 
 module.exports = router
