08e7205bb59de8ffebe5f61f4aa51c69041c62a7
wxf <1019072635@qq.com>
2023-01-25 23:52:39 +0800

    feat: 添加个人信息修改，密码修改，头像修改处理函数

--- router_handler/user.js
+++ router_handler/user.js
@@ -1,4 +1,5 @@
 const db = require('../db/index')
+const bcrypt = require('bcryptjs')
 
 exports.getUserInfo = (req, res) => {
   const uid = req.params.uid
@@ -16,4 +17,71 @@ exports.getUserInfo = (req, res) => {
       data: results[0],
     })
   })
+}
+exports.updateUserInfo = (req, res) => {
+  const uid = req.params.uid
+  const userinfo = req.body
+  const sql_update_userinfo = `update bb_users set ? where uid=?`
+  db.query(sql_update_userinfo,[userinfo, uid],(err,results) => {
+    if (err) {
+      return res.err(err)
+    }
+    if (results.affectedRows !== 1) {
+      return res.err('修改用户信息失败！')
+    }
+    res.send({
+      code: 20000,
+      message: '修改用户信息成功！',
+    })
+  })
+}
+exports.updatePassword = (req, res) => {
+  const uid = req.params.uid
+  const {oldPwd, newPwd} = req.body
+  const sql_select_userinfo = `select * from bb_users where uid=?`
+  db.query(sql_select_userinfo, uid, (err, results) => {
+    if (err) {
+      return res.err(err)
+    }
+    if (results.length !== 1) {
+      return res.err('用户不存在！')
+    }
+    // 判断旧密码是否正确
+    const compareResult = bcrypt.compareSync(oldPwd, results[0].password)
+    if (!compareResult) {
+      return res.err('原密码错误！')
+    }
+    const sql_update_password = `update bb_users set password=? where uid=?`
+    const newPassword = bcrypt.hashSync(newPwd, 10)
+    db.query(sql_update_password, [newPassword, uid], (err, results) => {
+      if (err) {
+        return res.err(err)
+      }
+      if (results.affectedRows !== 1) {
+        return res.err('更新密码失败！')
+      }
+      res.send({
+        code: 20000,
+        message: '更新密码成功！',
+      })
+    })
+  })
+}
+exports.updateAvatar = (req, res) => {
+  const uid = req.params.uid
+  const avatar = req.body.avatar
+  const sql_update_avatar = 'update bb_users set user_pic=? where uid=?'
+  db.query(sql_update_avatar, [avatar, uid], (err, results) => {
+    if (err) {
+      return res.err(err)
+    }
+    if (results.affectedRows !== 1) {
+      return res.err('更新头像失败！')
+    }
+    res.send({
+      code: 20000,
+      message: '更新密码成功！',
+    })
+  })
+
 }
