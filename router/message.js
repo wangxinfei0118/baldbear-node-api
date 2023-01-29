04561e9f0bc391bc72d87e6465f6c6a7c1631e00
wxf <1019072635@qq.com>
2023-01-30 02:28:14 +0800

    feat: 定义message相关表单数据校验

--- router/message.js
+++ router/message.js
@@ -1,11 +1,13 @@
 const express = require('express')
+const expressJoi = require('@escook/express-joi')
 
 const messageHandler = require('../router_handler/message')
+const { message_schema } = require('../schema/message')
 
 const router = express.Router()
 
 router.get('/message/', messageHandler.getMessage)
-router.post('/message', messageHandler.addMessage)
+router.post('/message', expressJoi(message_schema), messageHandler.addMessage)
 router.delete('/message/:id', messageHandler.deleteMessage)
 
 module.exports = router
---
+++ schema/message.js
@@ -0,0 +1,19 @@
+const joi = require('joi')
+
+const pid = joi.string().required().error(new Error('pid不能为空'))
+const user_id = joi.string().required().error(new Error('userId不能为空'))
+const nickname = joi.string().required().error(new Error('nickname不能为空'))
+const user_pic = joi.string().uri().error(new Error('图片url格式不正确'))
+const content = joi.string().required().error(new Error('内容不能为空'))
+
+
+exports.message_schema = {
+  body: {
+    pid,
+    user_id,
+    nickname,
+    user_pic,
+    content
+  }
+}
+
