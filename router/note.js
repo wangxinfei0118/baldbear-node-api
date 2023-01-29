2607c0222b3b781ef33a516d54a759ee37b3ecbd
wxf <1019072635@qq.com>
2023-01-30 02:28:42 +0800

    feat: 定义note相关表单数据校验规则

--- router/note.js
+++ router/note.js
@@ -1,14 +1,16 @@
 const express = require('express')
+const expressJoi = require('@escook/express-joi')
 
 const noteHandler = require('../router_handler/note')
+const { note_schema } = require('../schema/note')
 
 const router = express.Router()
 
 router.get('/label', noteHandler.getNoteLabel)
 router.get('/note', noteHandler.getNoteList)
 router.get('/note/:id', noteHandler.getNoteById)
-router.post('/note', noteHandler.addNote)
-router.put('/note/:id', noteHandler.editNote)
+router.post('/note', expressJoi(note_schema), noteHandler.addNote)
+router.put('/note/:id', expressJoi(note_schema), noteHandler.editNote)
 router.delete('/note/:id', noteHandler.deleteNote)
 router.put('/viewCount/:id', noteHandler.updateNoteViewCount)
 router.get('/comment/:id', noteHandler.getCommentByNoteId)
---
+++ schema/note.js
@@ -0,0 +1,36 @@
+const joi = require('joi')
+
+const title = joi.string().required().error(new Error('标题不能为空'))
+const summary = joi.string().required().error(new Error('摘要不能为空'))
+const label = joi.string().required().error(new Error('标签不能为空'))
+const image_url = joi.string().uri().required().error(new Error('图片url格式不正确'))
+const md_content = joi.string().required().error(new Error('mdContent不能为空'))
+const html_content = joi.string().required().error(new Error('htmlContent不能为空'))
+const pid = joi.string().required().error(new Error('pid不能为空'))
+const user_id = joi.string().required().error(new Error('userId不能为空'))
+const note_id = joi.string().required().error(new Error('noteId不能为空'))
+const nickname = joi.string().required().error(new Error('nickname不能为空'))
+const user_pic = joi.string().uri().error(new Error('图片url格式不正确'))
+const content = joi.string().required().error(new Error('内容不能为空'))
+
+exports.note_schema = {
+  body: {
+    title,
+    summary,
+    label,
+    image_url,
+    md_content,
+    html_content
+  }
+}
+
+exports.comment_schema = {
+  body: {
+    pid,
+    user_id,
+    note_id,
+    nickname,
+    user_pic,
+    content
+  }
+}
