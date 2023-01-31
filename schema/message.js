da3ac06faad0947500ec0407256f0aaa59cc4818
wxf <1019072635@qq.com>
2023-01-31 23:28:54 +0800

    fix: pid、userid验证类型修改

--- schema/message.js
+++ schema/message.js
@@ -1,7 +1,7 @@
 const joi = require('joi')
 
-const pid = joi.string().required().error(new Error('pid不能为空'))
-const user_id = joi.string().required().error(new Error('userId不能为空'))
+const pid = joi.number().required().error(new Error('pid不能为空'))
+const user_id = joi.number().required().error(new Error('userId不能为空'))
 const nickname = joi.string().required().error(new Error('nickname不能为空'))
 const user_pic = joi.string().uri().error(new Error('图片url格式不正确'))
 const content = joi.string().required().error(new Error('内容不能为空'))
