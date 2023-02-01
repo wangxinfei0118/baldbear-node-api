f47d4290b3ddd792179f4ef34f734ba28fe2ed4e
wxf <1019072635@qq.com>
2023-02-01 20:32:46 +0800

    feat: 添加cross-env修改开发和生成环境变量，并根据环境生成上传文件路径

--- router_handler/file.js
+++ router_handler/file.js
@@ -1,17 +1,19 @@
 const fs = require('fs')
-
+const path = require("path");
+const baseUrl = process.env.NODE_ENV === 'dev' ? 'http://localhost:3008' : 'http://server.baldbear.cn'
 exports.uploadImage = (req, res) => {
   if (!req.file){
-    res.err('上传图片失败！')
+    return res.err('上传图片失败！')
   }
+  const fileUrl = baseUrl + req.file.path.split('baldbear-node-api')[1].replace(/\\/g,'/')
   res.send({
     code: 20000,
     message: '上传图片成功！',
-    data: req.file.path
+    data: fileUrl
   })
 }
 exports.deleteImage = (req, res) => {
-  const fileUrl = req.query.fileUrl
+  const fileUrl = path.join(__dirname, '../static/uploads', req.query.fileUrl.split('uploads')[1])
   fs.unlink(fileUrl, (err) => {
     if (err) {
       return res.err(err)
