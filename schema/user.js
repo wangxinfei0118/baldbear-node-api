c8130aab36913a0a41c1370efa059ab8607b2341
wxf <1019072635@qq.com>
2023-02-01 20:29:53 +0800

    fix: 修改user相关验证规则

--- schema/user.js
+++ schema/user.js
@@ -4,13 +4,14 @@ const nickname = joi.string().error(new Error('昵称不合法'))
 const email = joi.string().email().error(new Error('邮箱格式不正确'))
 const phone = joi.string().pattern(/^1[3-9]\d{9}$/).error(new Error('手机号码不正确'))
 const password = joi.string().pattern(/^[\S]{6,18}$/).required().error(new Error('密码必须为6-18位字符'))
-const avatar = joi.string().dataUri().required()
+const user_pic = joi.string()
 
 exports.userinfo_schema = {
   body: {
     nickname,
     email,
-    phone
+    phone,
+    user_pic
   }
 }
 exports.update_password_schema = {
@@ -19,10 +20,6 @@ exports.update_password_schema = {
     new_pwd: joi.not(joi.ref('oldPwd')).concat(password).error(new Error('新密码不能和旧密码一样且必须为6-18位字符'))
   }
 }
-exports.update_avatar_schema = {
-  body: {
-    avatar
-  }
-}
+
 
 
