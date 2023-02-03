8f83ce943c4c19d2c5d42f846be7c243318a7853
wxf <1019072635@qq.com>
2023-02-03 21:27:25 +0800

    chore: async/await重构note代码

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -1,234 +1,121 @@
-const db = require('../db/index')
-const getTime = require('../utils/getTime')
-const arrToTree = require('../utils/arrToTree')
+const { to } = require("await-to-js");
+const noteService = require("../services/noteService");
 
 exports.getNoteLabel = async (req, res) => {
-  const sql_label = 'select * from bb_labels'
-  const sql_label_item = 'select * from bb_labels_item'
-  const labelList = await new Promise((resolve, reject) => {
-    db.query(sql_label, (err, results) => {
-      if (err) {
-        return res.err(err)
-      }
-      resolve(results)
-    })
-  })
-  const labelItemList = await new Promise((resolve, reject) => {
-    db.query(sql_label_item, (err, results) => {
-      if (err) {
-        return res.err(err)
-      }
-      resolve(results)
-    })
-  })
-  const data = arrToTree(labelList,labelItemList,'labelList')
+  const [err, labelList] = await to(noteService.getNoteLabel())
+  if (err){
+    return res.err(err)
+  }
   res.send({
     code: 20000,
     message: '获取笔记分类列表成功！',
-    data
+    data: labelList
   })
 }
-exports.getNoteList = (req, res) => {
-  const queryObj = req.body
-  const start = (queryObj.current - 1)*queryObj.size
-  // 根据queryObj.label是否存在 判断是否是根据标签查询
-  const sql_note_count = `select count(*) total from bb_notes ` + (queryObj.label ? `where label='${queryObj.label}'`:'')
-  const sql_note_list = `select note_id, title, summary, label, image_url, view_count, chat_count, create_date, update_date from bb_notes ` + (queryObj.label ? `where label='${queryObj.label}'`:'') + ` order by create_date desc limit ${start},${queryObj.size}`
-  const noteCount = new Promise((resolve, reject) => {
-    db.query(sql_note_count, (err, results) => {
-      if (err) {
-        reject(err)
-      }
-      resolve(results[0])
-    })
-  })
-  const noteList = new Promise((resolve, reject) => {
-    db.query(sql_note_list, (err, results) => {
-      if (err) {
-        reject(err)
-      }
-      resolve(results)
-    })
-  })
-  Promise.all([noteCount,noteList]).then(results => {
-    res.send({
-      code: 20000,
-      message: '获取笔记列表成功！',
-      data: {
-        total: results[0].total,
-        records: results[1]
-      }
-    })
-  }).catch(err =>{
-    res.err(err)
-  })
-}
-exports.getNoteById = (req, res) => {
-  const noteId = req.params.id
-  const sql_note_item = 'select * from bb_notes where note_id=?'
-  db.query(sql_note_item, noteId, (err, results) => {
-    if (err) {
-      return res.err(err)
+exports.getNoteList = async (req, res) => {
+  const [err, results] = await to(noteService.getNoteList(req.body))
+  if (err){
+    return res.err(err)
+  }
+  res.send({
+    code: 20000,
+    message: '获取笔记列表成功！',
+    data: {
+      total: results[0].total,
+      records: results[1]
     }
-    if (results.length !==1){
-      return res.err('获取笔记详情失败！')
-    }
-    res.send({
-      code: 20000,
-      message: '获取笔记详情成功！',
-      data: results[0]
-    })
   })
 }
-exports.addNote = (req, res) => {
+exports.getNoteById = async (req, res) => {
+  const [err, noteDetail] = await to(noteService.getNoteById(req.params.id))
+  if (err){
+    return res.err(err)
+  }
+  res.send({
+    code: 20000,
+    message: '获取笔记详情成功！',
+    data: noteDetail
+  })
+}
+exports.addNote = async (req, res) => {
   if (req.user.role !== 1){
     return res.err('暂无权限')
   }
-  const noteData = req.body
-  const sql_insert_note = 'insert into bb_notes set ?'
-  db.query(sql_insert_note, {title: noteData.title, summary: noteData.summary, label: noteData.label, image_url: noteData.image_url, view_count: 0, chat_count: 0, md_content: noteData.md_content, html_content: noteData.html_content, create_date: getTime(), update_date: getTime()}, (err, results) => {
-    if (err) {
-      return res.err(err)
-    }
-    if (results.affectedRows !== 1) {
-      return res.err('新增笔记失败！')
-    }
-    res.send({
-      code: 20000,
-      message: '新增笔记成功！',
-      data: results.insertId
-    })
+  const [err, noteId] = await to(noteService.addNote(req.body))
+  if (err){
+    return res.err(err)
+  }
+  res.send({
+    code: 20000,
+    message: '新增笔记成功！',
+    data: noteId
   })
 }
-exports.editNote = (req, res) => {
+exports.editNote = async (req, res) => {
   if (req.user.role !== 1){
     return res.err('暂无权限')
   }
-  const noteId = req.params.id
-  const noteData = req.body
-  noteData.update_date = getTime()
-  const sql_edit_note = 'update bb_notes set ? where note_id=?'
-  db.query(sql_edit_note, [noteData, noteId], (err, results) => {
-    if (err) {
-      return res.err(err)
-    }
-    if (results.affectedRows !== 1) {
-      return res.err('修改笔记失败！')
-    }
-    res.send({
-      code: 20000,
-      message: '修改笔记成功！'
-    })
+  const [ err, noteId ] = await to(noteService.editNote(req.params.id, req.body))
+  if (err){
+    return res.err(err)
+  }
+  res.send({
+    code: 20000,
+    message: '修改笔记成功！',
+    data: noteId
   })
 }
-exports.deleteNote = (req, res) => {
+exports.deleteNote = async (req, res) => {
   if (req.user.role !== 1){
     return res.err('暂无权限')
   }
-  const noteId = req.params.id
-  const sql_delete_note = 'delete from bb_notes where note_id=?'
-  db.query(sql_delete_note, noteId, (err, results) => {
-    if (err) {
-      return res.err(err)
-    }
-    if (results.affectedRows !== 1) {
-      return res.err('删除笔记失败！')
-    }
-    res.send({
-      code: 20000,
-      message: '删除笔记成功！'
-    })
+  const [ err ] = await to(noteService.deleteNote(req.params.id))
+  if (err){
+    return res.err(err)
+  }
+  res.send({
+    code: 20000,
+    message: '删除笔记成功！'
   })
 }
-exports.updateNoteViewCount = (req, res) => {
-  const noteId = req.params.id
-  const sql_note_list = 'update bb_notes set view_count=view_count+1 where note_id=?'
-  db.query(sql_note_list, noteId, (err, results) => {
-    if (err) {
-      return res.err(err)
-    }
-    if (results.affectedRows !== 1) {
-      return res.err('更新浏览数失败！')
-    }
-    res.send({
-      code: 20000,
-      message: '更新浏览数成功！'
-    })
+exports.updateNoteViewCount = async (req, res) => {
+  const [ err ] = await to(noteService.updateNoteViewCount(req.params.id))
+  if (err){
+    return res.err(err)
+  }
+  res.send({
+    code: 20000,
+    message: '更新浏览数成功！'
   })
 }
-exports.getCommentByNoteId = (req, res) => {
-  const noteId = req.params.id
-  const sql_comment = 'select * from bb_note_comment where note_id=?'
-  db.query(sql_comment, noteId, (err, results) => {
-    if (err) {
-      return res.err(err)
-    }
-    const data = arrToTree(results)
-    res.send({
-      code: 20000,
-      message: '获取笔记详情成功！',
-      data
-    })
+exports.getCommentByNoteId = async (req, res) => {
+  const [ err, commentList] = await to(noteService.getCommentByNoteId(req.params.id))
+  if (err){
+    return res.err(err)
+  }
+  res.send({
+    code: 20000,
+    message: '获取笔记评论成功！',
+    data: commentList
   })
 }
-exports.addComment = (req, res) => {
-  const commentData = req.body
-  const sql_insert_comment = 'insert into bb_note_comment set ?'
-  db.query(sql_insert_comment, {pid: commentData.pid, user_id: commentData.user_id, note_id: commentData.note_id, nickname: commentData.nickname, below_reply_id: commentData.below_reply_id, below_reply_name: commentData.below_reply_name, user_pic: commentData.user_pic, content: commentData.content, create_date: getTime()}, (err, results) => {
-    if (err) {
-      return res.err(err)
-    }
-    if (results.affectedRows !== 1) {
-      return res.err('新增评论失败！')
-    }
-    const sql_add_comment_count = 'update bb_notes set chat_count=chat_count+1 where note_id=?'
-    db.query(sql_add_comment_count, commentData.note_id, (err, results) => {
-      if (err) {
-        return res.err(err)
-      }
-      if (results.affectedRows !== 1) {
-        return res.err('更新评论数失败！')
-      }
-      res.send({
-        code: 20000,
-        message: '新增评论成功！'
-      })
-    })
+exports.addComment = async (req, res) => {
+  const [ err ] = await to(noteService.addComment(req.body))
+  if (err){
+    return res.err(err)
+  }
+  res.send({
+    code: 20000,
+    message: '新增评论成功！'
   })
 }
-exports.deleteComment = (req, res) => {
-  const commentId = req.params.id
-  const sql_select_note = 'select note_id from bb_note_comment where id=?'
-  db.query(sql_select_note, commentId, (err, results) => {
-    if (err) {
-      return res.err(err)
-    }
-    const uid = results[0].user_id
-    const note_id = results[0].note_id
-    if (req.user.uid !== uid && req.user.role !== 1) {
-      return res.err('暂无权限')
-    }
-    const sql_delete_comment = 'delete from bb_note_comment where id=?'
-    db.query(sql_delete_comment, commentId, (err, results) => {
-      if (err) {
-        return res.err(err)
-      }
-      if (results.affectedRows !== 1) {
-        return res.err('删除评论失败！')
-      }
-      const sql_reduce_comment_count = 'update bb_notes set chat_count=chat_count-1 where note_id=?'
-      db.query(sql_reduce_comment_count, note_id, (err, results) => {
-        if (err) {
-          return res.err(err)
-        }
-        if (results.affectedRows !== 1) {
-          return res.err('更新评论数失败！')
-        }
-        res.send({
-          code: 20000,
-          message: '删除评论成功！'
-        })
-      })
-    })
+exports.deleteComment = async (req, res) => {
+  const [ err ] = await to(noteService.deleteComment(req.params.id, req.user.uid, req.user.role))
+  if (err){
+    return res.err(err)
+  }
+  res.send({
+    code: 20000,
+    message: '删除评论成功！'
   })
 }
---
+++ services/noteService.js
@@ -0,0 +1,210 @@
+const db = require("../db");
+const arrToTree = require("../utils/arrToTree");
+const getTime = require("../utils/getTime");
+
+exports.getNoteLabel = async () => {
+  const sql_label = 'select * from bb_labels'
+  const sql_label_item = 'select * from bb_labels_item'
+  const labelList = await new Promise((resolve, reject) => {
+    db.query(sql_label, (err, results) => {
+      if (err) {
+        reject(err)
+      }
+      resolve(results)
+    })
+  })
+  const labelItemList = await new Promise((resolve, reject) => {
+    db.query(sql_label_item, (err, results) => {
+      if (err) {
+        reject(err)
+      }
+      resolve(results)
+    })
+  })
+  return arrToTree(labelList,labelItemList,'labelList') // 转成树状结构
+}
+exports.getNoteList = async (queryObj) => {
+  // 开始查询的index
+  const start = (queryObj.current - 1) * queryObj.size
+  // 查询笔记总数
+  const sql_note_count = `select count(*) total from bb_notes ` + (queryObj.label ? `where label='${queryObj.label}'`:'')
+  // 根据queryObj.label是否存在 判断是否是根据标签查询
+  const sql_note_list = `select note_id, title, summary, label, image_url, view_count, chat_count, create_date, update_date from bb_notes ` + (queryObj.label ? `where label='${queryObj.label}'`:'') + ` order by create_date desc limit ${start},${queryObj.size}`
+  const noteCount = new Promise((resolve, reject) => {
+    db.query(sql_note_count, (err, results) => {
+      if (err) {
+        reject(err)
+      }
+      resolve(results[0])
+    })
+  })
+  const noteList = new Promise((resolve, reject) => {
+    db.query(sql_note_list, (err, results) => {
+      if (err) {
+        reject(err)
+      }
+      resolve(results)
+    })
+  })
+  return await Promise.all([noteCount,noteList])
+}
+exports.getNoteById = async (noteId) => {
+  const sql_note_item = 'select * from bb_notes where note_id=?'
+  return await new Promise((resolve, reject) => {
+    db.query(sql_note_item, noteId, (err, results) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      if (results.length !==1){
+        reject('获取笔记详情失败！')
+      }
+      resolve(results[0])
+    })
+  })
+}
+exports.addNote = async (noteData) => {
+  noteData.create_date = noteData.update_date = getTime()
+  noteData.view_count = noteData.chat_count = 0
+  const sql_insert_note = 'insert into bb_notes set ?'
+  return await new Promise((resolve, reject) => {
+    db.query(sql_insert_note, noteData, (err, results) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      if (results.affectedRows !== 1) {
+        reject('新增笔记失败！')
+      }
+      resolve(results.insertId)
+    })
+  })
+}
+exports.editNote = async (noteId, noteData) => {
+  noteData.update_date = getTime()
+  const sql_edit_note = 'update bb_notes set ? where note_id=?'
+  await new Promise((resolve, reject) => {
+    db.query(sql_edit_note, [noteData, noteId], (err, results) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      if (results.affectedRows !== 1) {
+        reject('修改笔记失败！')
+      }
+      resolve()
+    })
+  })
+}
+exports.deleteNote = async (noteId) => {
+  const sql_delete_note = 'delete from bb_notes where note_id=?'
+  await new Promise((resolve, reject) => {
+    db.query(sql_delete_note, noteId, (err, results) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      if (results.affectedRows !== 1) {
+        reject('删除笔记失败！')
+      }
+      resolve()
+    })
+  })
+}
+exports.updateNoteViewCount = async (noteId) => {
+  const sql_note_list = 'update bb_notes set view_count=view_count+1 where note_id=?'
+  await new Promise((resolve, reject) => {
+    db.query(sql_note_list, noteId, (err, results) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      if (results.affectedRows !== 1) {
+        reject('更新浏览数失败！')
+      }
+      resolve()
+    })
+  })
+}
+exports.getCommentByNoteId = async (noteId) => {
+  const sql_comment = 'select * from bb_note_comment where note_id=?'
+  return await new Promise((resolve, reject) => {
+    db.query(sql_comment, noteId, (err, results) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      const commentList = arrToTree(results)
+      resolve(commentList)
+    })
+  })
+}
+exports.addComment = async (commentData) => {
+  const sql_insert_comment = 'insert into bb_note_comment set ?'
+  await new Promise((resolve, reject) => {
+    db.query(sql_insert_comment, {pid: commentData.pid, user_id: commentData.user_id, note_id: commentData.note_id, nickname: commentData.nickname, below_reply_id: commentData.below_reply_id, below_reply_name: commentData.below_reply_name, user_pic: commentData.user_pic, content: commentData.content, create_date: getTime()}, (err, results) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      if (results.affectedRows !== 1) {
+        reject('新增评论失败！')
+      }
+      resolve()
+    })
+  })
+  const sql_add_comment_count = 'update bb_notes set chat_count=chat_count+1 where note_id=?'
+  await new Promise((resolve, reject) => {
+    db.query(sql_add_comment_count, commentData.note_id, (err, results) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      if (results.affectedRows !== 1) {
+        reject('更新评论数失败！')
+      }
+      resolve()
+    })
+  })
+}
+exports.deleteComment = async (commentId, uid, role) => {
+  const sql_select_note = 'select note_id from bb_note_comment where id=?'
+  const note_id = await new Promise((resolve, reject) => {
+    db.query(sql_select_note, commentId, (err, results) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      if (uid !== results[0].user_id && role !== 1) {
+        reject('暂无权限')
+      }
+      resolve(results[0].note_id)
+    })
+  })
+  await new Promise((resolve, reject) => {
+    const sql_delete_comment = 'delete from bb_note_comment where id=?'
+    db.query(sql_delete_comment, commentId, (err, results) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      if (results.affectedRows !== 1) {
+        reject('删除评论失败！')
+      }
+      resolve()
+    })
+  })
+  await new Promise((resolve, reject) => {
+    const sql_reduce_comment_count = 'update bb_notes set chat_count=chat_count-1 where note_id=?'
+    db.query(sql_reduce_comment_count, note_id, (err, results) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      if (results.affectedRows !== 1) {
+        reject('更新评论数失败！')
+      }
+      resolve()
+    })
+  })
+}
