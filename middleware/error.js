c833beb2746607d31bd11060d877036a116632b8
wxf <1019072635@qq.com>
2023-02-03 21:29:14 +0800

    修改错误code

--- middleware/error.js
+++ middleware/error.js
@@ -5,15 +5,14 @@ const joi = require("joi");
  * 401000 身份认证失败 无token
  * 401001 用户无权限进行操作
  * 50000 业务自定义错误
- * 50001 未知错误
+ * 50001 表单验证失败
  */
 
 const error = function (err, req, res, next) {
   // 表单验证失败
-  if (err instanceof joi.ValidationError) return res.err('表单验证失败')
+  if (err instanceof joi.ValidationError) return res.err('表单验证失败',50001)
   // 身份认证失败
   if (err.name === 'UnauthorizedError') return res.err('身份认证失败，清重新登录',40100)
-  // 未知错误
-  res.err(err,50001)
+  res.err(err)
 }
 module.exports = error
