d4f852f9f4f466c1fca027963d93ec9e41d0b3f8
wxf <1019072635@qq.com>
2023-02-03 21:21:44 +0800

    fix: 修改note、life相关验证规则

--- schema/lifeSchema.js
+++ schema/lifeSchema.js
@@ -1,12 +1,14 @@
 const joi = require('joi')
 
 const title = joi.string().required().error(new Error('标题不能为空'))
+const md_content = joi.string().required().error(new Error('mdContent不能为空'))
 const html_content = joi.string().required().error(new Error('内容不能为空'))
 
 
 exports.life_schema = {
   body: {
     title,
+    md_content,
     html_content,
   }
 }
